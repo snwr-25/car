ffbba1e6eb80849c9609288c8d9699d3ae7642b6
snwr-25 <snwrphoto4@gmail.com>
2025-08-11 16:39:22 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
             departmentDiv.innerHTML = `
                 <div class="department-header" data-target="${deptId}">
                     <h2>${deptName} (${vehiclesInDept.length})</h2>
-                    <span class="arrow">►</span>
+                    <span class="arrow"></span>
                 </div>
                 <div class="department-content" id="${deptId}">
                     <table class="vehicle-table">
@@ -73,12 +73,12 @@ document.addEventListener('DOMContentLoaded', () => {
                         <tbody>
                             ${vehiclesInDept.map(vehicle => `
                                 <tr data-id="${vehicle.id}">
-                                    <td>${vehicle.vehicleType || ''}</td>
-                                    <td>${vehicle.vehicleNumber || ''}</td>
-                                    <td>${vehicle.vehicleModel || ''}</td>
-                                    <td>${vehicle.recipientParty || ''}</td>
-                                    <td>${vehicle.userName || ''}</td>
-                                    <td class="actions">
+                                    <td data-label="جۆری ئۆتۆمبێل">${vehicle.vehicleType || ''}</td>
+                                    <td data-label="ژمارە">${vehicle.vehicleNumber || ''}</td>
+                                    <td data-label="مۆدێل">${vehicle.vehicleModel || ''}</td>
+                                    <td data-label="لایەنی وەرگر">${vehicle.recipientParty || ''}</td>
+                                    <td data-label="بەکارهێنەر">${vehicle.userName || ''}</td>
+                                    <td data-label="کردارەکان" class="actions">
                                         <button class="btn btn-sm btn-info view-btn" title="بینین"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/><path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/></svg></button>
                                         <button class="btn btn-sm btn-success edit-btn" title="دەستکاری"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/></svg></button>
                                         <button class="btn btn-sm btn-warning transfer-btn" title="گواستنەوە"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.347a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5a1.5 1.5 0 0 1-1.5-1.5V2a.5.5 0 0 0-.5-.5"/></svg></button>
@@ -249,7 +249,6 @@ document.addEventListener('DOMContentLoaded', () => {
             input.addEventListener('input', updateTotalExpenses);
         });
         DOM.expenseList.appendChild(item);
-        item.style.gridTemplateColumns = '1fr 1fr 1fr 2fr auto';
     }
 
     function updateTotalExpenses() {
